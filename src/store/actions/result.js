c70c8f74131a88837b37ea9155f117a00ffe79d1
Lypzis <victorvp2008@hotmail.com>
2019-09-25 23:34:19 -0300

    Update actions structure

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,17 +1,20 @@
 import * as actionTypes from './actionTypes';
 
 export const saveResult = result => {
+    //const updatedResult = result * 2; 
     return {
         type: actionTypes.STORE_RESULT,
-        result
+        result //: updatedResult
     }
 }
 
 export const storeResult = result => {
     // this is only possible WITH redux-thunk middleware
-    return dispatch =>
+    return dispatch => //'getState' second argument gets state, case NEEDED, DON'T OVERUSE
         // makes possible to run asynchronous code
         setTimeout(() => {
+            //const oldCounter = getState().ctr.counter;
+            //console.log(oldCounter);
             dispatch(saveResult(result));
         }, 2000);
 }
